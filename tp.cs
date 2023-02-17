ffeef7ea26deaf74f0efb25735ded2f7ff1e70e1
Kaludjerovic <Kaludjerovic@DESKTOP-021QBB7>
2023-02-17 18:59:16 +0100

    proverka

---
+++ tp.cs
@@ -0,0 +1,18 @@
+// Пространство имён
+namespace Lesson1
+{
+    /// <summary>
+    /// Главный класс - Program
+    /// </summary>
+    internal class Program
+    {
+        /// <summary>
+        /// Точка входа - метод, исполняемый при запуске программы.
+        /// Он всегда называется Main
+        /// </summary>
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
